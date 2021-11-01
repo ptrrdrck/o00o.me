90490265450b763d7a2caa964046da74677be3a3
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-10-31 23:13:53 -0400

    Create random string script

---
+++ o00o.js
@@ -0,0 +1,14 @@
+const characters = 'o0';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+var oO = generateString(1000);
+
+document.getElementById("rand").innerHTML = oO;
