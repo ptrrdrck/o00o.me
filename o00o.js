ff46279bd3f22f7829e59b6f5085053c3a10327d
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-11-10 18:56:43 -0500

    Create max repeat function

--- o00o.js
+++ o00o.js
@@ -9,6 +9,26 @@ function makeString(length) {
     return result;
 }
 
-var oO = makeString(1000);
+var o0 = makeString(1000);
 
-document.getElementById("rand").innerHTML = oO;
+document.getElementById("rand").innerHTML = o0;
+
+
+function findMaxRepeat(string) {
+    let obj = {};
+    let maxLength = 0;
+    let start = 0;
+    const stringArray = string.split('');
+    for (let i = 0; i < string.length; i++) {
+        if (obj[stringArray[i]] !== undefined && obj[stringArray[i]] >= start && obj[stringArray[i]] == obj[stringArray[(i + 1)]]) {
+            start = obj[stringArray[i]] + 1;
+        }
+        obj[stringArray[i]] = i;
+        maxLength = Math.max(maxLength, (i - start + 1))
+    }
+    return maxLength;
+}
+
+var maxRepeat = findMaxRepeat(o0);
+
+document.getElementById("max").innerHTML = `Your longest repetition is ${maxRepeat} characters.`;
